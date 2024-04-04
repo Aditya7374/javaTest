28768d6b20d5bd5e8a88abf03e626f4e3f8b2c46
Aditya7374 <166002614+Aditya7374@users.noreply.github.com>
2024-04-04 15:25:42 +0530

    Update que2.java

--- que2.java
+++ que2.java
@@ -1,3 +1,9 @@
+/*
+Q2:Find maximum sum path involving elements of given arrays
+Given two sorted arrays of integers, find a maximum sum path involving elements of both arrays whose sum is maximum. 
+We can start from either array, but we can switch between arrays only through its common elements.
+*/
+
 public class MaximumSumPath {
 
     public static int findMaxSumPath(int[] X, int[] Y) {
