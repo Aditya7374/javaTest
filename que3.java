90410b8ebb90b58118af208a72f3b9fbd46f2c99
Aditya7374 <166002614+Aditya7374@users.noreply.github.com>
2024-04-04 15:28:15 +0530

    Update que3.java

--- que3.java
+++ que3.java
@@ -1,11 +1,16 @@
+/*
+Q3:Write a Java Program to count the number of words in a string using HashMap.
+
+*/
+
 public class WordCount {
       static int wordcount(String string)                                                                                       {
-          int count=0;
+   int count=0;
 
-            char ch[]= new char[string.length()];
-            for(int i=0;i<string.length();i++)
-            {                                                                                                                           ch[i]= string.charAt(i);
-                if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) )                                                       count++;
+   char ch[]= new char[string.length()];
+            for(inti=0;i<string.length();i++)
+        {                                            ch[i]= string.charAt(i);
+   if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) )                                                       count++;
             }
             return count;                                                                                                       }                                                                                                                     public static void main(String[] args) {
           String string ="    India Is My Country";
